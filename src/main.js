ad7e1ddd7db9492938d77fee9ae26cb9d1ea558f
Nicole Lopes <nicole.cls25@gmail.com>
2024-11-26 19:52:11 -0300

    Dependencia de icones

--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'  
-import VueRouter from 'vue-router' // Importando o Vue Router
-import { routes } from './router' // Importando as rotas do arquivo router
+import Vue from 'vue';
+import App from './App.vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';  
+import router from './router'; 
+import '@mdi/font/css/materialdesignicons.css';
 
-Vue.config.productionTip = false
 
-Vue.use(Vuetify)
-Vue.use(VueRouter) // Registrando o Vue Router
+Vue.config.productionTip = false;
 
-const router = new VueRouter({
-  routes, // Usando as rotas definidas
-  mode: 'history' // Para evitar o # nas URLs
-})
+Vue.use(Vuetify);
 
 new Vue({
   render: h => h(App),
   vuetify: new Vuetify(), 
-  router // Adicionando o Vue Router ao Vue
-}).$mount('#app')
+  router, 
+}).$mount('#app');
