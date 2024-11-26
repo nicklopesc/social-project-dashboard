9882b3f1f63d644e67bbf86c8e9628bb4d84442d
Nicole Lopes <nicole.cls25@gmail.com>
2024-11-26 19:52:34 -0300

    Ajuste de rota

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,54 @@
-// src/router.js
-import DashboardHome from '@/views/DashboardHome.vue'
-import ImpactOverview from '@/views/home/components/ImpactOverview.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// import DefaultLayout from '@/layout/DefaultLayout.vue';
+import DashboardLayout from '@/layout/DashboardLayout.vue';
+import Home from '@/views/DashboardHome.vue';
+import CadastrarProjeto from '@/views/register/CadastrarProjeto.vue';
+import ProjetosDisponiveis from '@/views/projects/ProjetosDisponiveis.vue';
+
+Vue.use(VueRouter);
 
 export const routes = [
+
   {
     path: '/',
-    name: 'Home',
-    component: DashboardHome
+    component: DashboardLayout, 
+    children: [
+      {
+        path: '', // Rota raiz
+        name: 'Home',
+        component: Home,
+      },
+      {
+        path: 'dashboard/cadastrar-projeto',
+        name: 'CadastrarProjeto',
+        component: CadastrarProjeto,
+      },
+      {
+        path: 'dashboard/projetos-disponiveis',
+        name: 'ProjetosDisponiveis',
+        component: ProjetosDisponiveis,
+      },
+    ],
   },
-  {
-    path: '/impact',
-    name: 'ImpactOverview',
-    component: ImpactOverview
-  }
-]
+  
+  // {
+  //   path: '/public',
+  //   component: DefaultLayout,
+  //   children: [
+  //     {
+  //       path: '', 
+  //       name: 'PublicPage',
+  //       component: () => import('@/views/PublicPage.vue'),
+  //     },
+  //   ],
+  // },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
+export default router;
