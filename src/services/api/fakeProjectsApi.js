43ff5a94a8caad1758a0a65ffa3bce4a0cfa152f
Nicole Lopes <nicole.cls25@gmail.com>
2024-11-26 20:53:09 -0300

    Api fake

---
+++ src/services/api/fakeProjectsApi.js
@@ -0,0 +1,84 @@
+import energiaSolarImage from '@/assets/images/energia.jpg';
+import saudeDigitalImage from '@/assets/images/telemedicina.jpg';
+import ecommerceInteligenteImage from '@/assets/images/commerce.jpg';
+import agricultura4Image from '@/assets/images/agricultura.jpg';
+import educacaoInclusivaImage from '@/assets/images/inclusive.jpg';
+import lerTransformarImage from '@/assets/images/ler.jpg';
+import aprendizadoDigitalImage from '@/assets/images/digital.jpg';
+import resgateCulturalImage from '@/assets/images/dance.jpg';
+
+
+export const projects = [
+    {
+      id: 1,
+      title: "Projeto Energia Solar",
+      description: "Desenvolvimento de um sistema sustentável para geração de energia solar.",
+      image: energiaSolarImage, 
+      area: "Energia",
+      creationDate: "2023-11-01",
+      targetFunding: 150000,
+    },
+    {
+      id: 2,
+      title: "Plataforma de Saúde Digital",
+      description: "Uma solução integrada para telemedicina e acompanhamento remoto.",
+      image: saudeDigitalImage, 
+      area: "Saúde",
+      creationDate: "2024-01-15",
+      targetFunding: 250000,
+    },
+    {
+      id: 3,
+      title: "E-commerce Inteligente",
+      description: "Plataforma com IA para personalização de compras online.",
+      image: ecommerceInteligenteImage, 
+      area: "Tecnologia",
+      creationDate: "2024-03-12",
+      targetFunding: 120000,
+    },
+    {
+      id: 4,
+      title: "Agricultura 4.0",
+      description: "Sistema de IoT para monitoramento de plantações em tempo real.",
+      image: agricultura4Image, 
+      area: "Agronegócio",
+      creationDate: "2023-08-25",
+      targetFunding: 180000,
+    },
+    {
+      id: 5,
+      title: "Educação Inclusiva",
+      description: "Plataforma para capacitar professores a trabalharem com crianças com necessidades especiais.",
+      image: educacaoInclusivaImage, 
+      area: "Educação",
+      creationDate: "2024-02-10",
+      targetFunding: 80000,
+    },
+    {
+      id: 6,
+      title: "Ler é Transformar",
+      description: "Projeto para incentivar a leitura em comunidades de baixa renda com bibliotecas móveis.",
+      image: lerTransformarImage, 
+      area: "Educação",
+      creationDate: "2023-09-05",
+      targetFunding: 60000,
+    },
+    {
+      id: 7,
+      title: "Aprendizado Digital",
+      description: "Criação de cursos gratuitos de programação para jovens em situação de vulnerabilidade.",
+      image: aprendizadoDigitalImage, 
+      area: "Educação",
+      creationDate: "2023-12-20",
+      targetFunding: 100000,
+    },
+    {
+      id: 8,
+      title: "Resgate Cultural",
+      description: "Projeto para revitalizar danças e músicas tradicionais esquecidas em comunidades rurais.",
+      image: resgateCulturalImage, 
+      area: "Cultura",
+      creationDate: "2024-01-10",
+      targetFunding: 75000,
+    },
+];
